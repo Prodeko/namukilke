bdcde4819b9dd86e8de09afe1ecd165ad195ba2e
sippola <mikko.sippola@dealdash.com>
2018-01-30 19:01:11 +0200

    initial model structure

--- namu/models.py
+++ namu/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+
+
+class Product(model.Models):
+
+
+class Transaction(model.Models):
+
+
+class Deposit(model.Models):
+
+
+class Restock(model.Models):
