bf39174041d82864d4799cfcaeb090bae0daf866
otsohellman <otso.hellman@gmail.com>
2018-05-28 19:18:33 +0300

    Users and items can now be added

--- namu/models.py
+++ namu/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
--- namu/urls.py
+++ namu/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
     path('id/<int:pk>/', views.user, name='user'),
-    path('id/<int:pk>/buy', views.Buy.as_view(), name='buy'),
+    path('id/<int:user_id>/buy', views.Buy.as_view(), name='buy'), #<-- tälleen tehään eikä pelkkä pk
     path('id/<int:pk>/deposit', views.Deposit.as_view(), name='deposit'),
 ]
