6b6292517cfdbdff97750aa11b8e3f6057d92808
sippola <mikko.sippola@dealdash.com>
2018-07-01 00:45:42 +0300

    user selection, product purchasing, code cleanup

--- namu/urls.py
+++ namu/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
-    path('id/', views.redirectToIndex, name='redirectToIndex'),
-    path('id/<int:user_id>/', views.redirectToBuy, name='redirectToBuy'),
+    path('id/', views.redirect_to_index, name='redirectToIndex'),
+    path('id/<int:user_id>/', views.redirect_to_buy, name='redirectToBuy'),
     path('id/<int:user_id>/buy', views.Buy.as_view(), name='buy'),
     path('id/<int:user_id>/deposit', views.Deposit.as_view(), name='deposit'),
 ]
