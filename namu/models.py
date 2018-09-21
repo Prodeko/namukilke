cc512d7b66e84e0e7dea9cd35f38822dc9120414
Mikko Sippola <19402452+sippola@users.noreply.github.com>
2018-09-21 16:05:50 +0300

    static img

--- namu/models.py
+++ namu/models.py
@@ -36,7 +36,7 @@ class Product(models.Model):
         default='c'
     )
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    picture = models.URLField()
+    picture = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
