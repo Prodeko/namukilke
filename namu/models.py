c5abf26a2e50f55140e0529567f67a0250e381c5
Mikko Sippola <19402452+sippola@users.noreply.github.com>
2018-09-21 19:09:53 +0300

    try blank=true for pics

--- namu/models.py
+++ namu/models.py
@@ -36,7 +36,7 @@ class Product(models.Model):
         default='c'
     )
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    picture = models.CharField(max_length=20)
+    picture = models.CharField(max_length=20, blank=True)
 
     def __str__(self):
         return self.name
