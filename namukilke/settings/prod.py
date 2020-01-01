919556614a400d0a1018b27e6722a51751bec66f
rriski <timo.h.riski@outlook.com>
2020-01-02 00:06:26 +0200

    Fix ALLOWED_HOSTS

--- namukilke/settings/prod.py
+++ namukilke/settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["namu.azurewebsites.net", "namu.prodeko.org", "127.0.0.1"]
+ALLOWED_HOSTS = ["namukilke.azurewebsites.net", "namu.prodeko.org", "127.0.0.1"]
 
 DB_USER = os.environ.get("DB_USER")
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
