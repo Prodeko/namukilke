33da39ce767c32884860b8e6734803ea86f2620b
Timo Riski <timo.h.riski@aalto.fi>
2020-09-23 23:47:34 +0300

    Remove unused import

--- namukilke/azure_backend.py
+++ namukilke/azure_backend.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from storages.backends.azure_storage import AzureStorage
 from django.contrib.staticfiles.storage import ManifestFilesMixin
-from azure.common import AzureMissingResourceHttpError
 
 
 class AzureMediaStorage(AzureStorage):
